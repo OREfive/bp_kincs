226fffb6de3cf8c7a948148c06b213a8204fd766
OREfive <148065563+OREfive@users.noreply.github.com>
2026-08-07 12:24:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'kincskereso-v1';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.hostname.indexOf('tile.openstreetmap.org') >= 0) {
+    e.respondWith(caches.open('kincskereso-tiles').then(c =>
+      c.match(req).then(hit => hit || fetch(req).then(r => { c.put(req, r.clone()); return r; }).catch(() => hit))));
+    return;
+  }
+  e.respondWith(caches.match(req).then(hit => hit || fetch(req).then(r => {
+    if (url.origin === location.origin) caches.open(CACHE).then(c => c.put(req, r.clone()));
+    return r.clone();
+  }).catch(() => caches.match('./index.html'))));
+});
